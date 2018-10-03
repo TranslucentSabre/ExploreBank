b0888fb6254492551e7807042aa5d27ef68db578
Timothy Myers <temyers240@gmail.com>
2018-10-03 14:01:24 -0500

    Celestial body values are complete. They may be moved later.

--- load.py
+++ load.py
@@ -15,44 +15,97 @@ from l10n import Locale
 
 VERSION = '0.01'
 
-BODIES = { "OStar" : (6135, 2556),
-           "BStar" : (3012, 1255),
-           "AStar" : (2949, 1226),
-           "FStar" : (2932, 1222),
-           "GStar" : (2919, 1216),
-           "KStar" : (2916, 1216),
-           "MStar" : (2903, 1208),
-           "WolfRaynet" : (2931, 1221),
-           "Herbig" : (3077, 1282),
-           "LStar" : (2889, 1204),
-           "TStar" : (2895, 1208),
-           "YStar" : (2881, 1200),
-           "Neutron" : (54782, 22814),
-           "DStar" : (34294, 14289),
-           "BlackHole" : (60589, 25819),
-           "GiantStar" : (3122, 1301),
-           "CarbonStar" : (2930, 1222)
-           "GasGiantWAmmoniaL" : (1721, 717),
-           "GasGiantWWaterL" : (2314, 964),
-           "ClassIGiant" : (7013, 2922),
-           "ClassIIGiant" : (53663, 22360),
-           "ClassIIIGiant" : (2693, 1122),
-           "ClassIVGiant" : (2799, 1166),
-           "ClassVGiant" : (2761, 1150),
-           "WaterGiant" : (1824, 760)
-           "HeGiants" : (2095, 986),
-           "HMC" : (34310, 13866, 412249, 171770),
-           "TWW" : (301410, 125587, 694971, 289587),
-           "MRB" : (65045, 27102),
-           "ELW" : (627885, 261619),
-           "Ammonia" : (320203, 133418),
-           "Icy" : (1246, 535),
-           "Rocky" : (928, 500, 181104, 75460)
+#Indices
+DETAILED_INDEX = 0
+BASIC_INDEX = 1
+#Stars
+T_TTauri = (2895, 1208)
+WolfRaynet = (2931, 1221)
+Carbon = (2930, 1222)
+WhiteDwarf = (34294, 14289)
+BlackHole = (60589, 25819)
+GiantStar = (3122, 1301)
+#Planets
+WaterGiant = (1824, 760)
+HeGiant = (2095, 986)
+IcyBody = (1246, 535)
+BODIES = { 
+# Stars
+           "O" : (6135, 2556),
+           "B" : (3012, 1255),
+           "A" : (2949, 1226),
+           "F" : (2932, 1222),
+           "G" : (2919, 1216),
+           "K" : (2916, 1216),
+           "M" : (2903, 1208),
+           "L" : (2889, 1204),
+           "T" : T_TTauri,
+           "Y" : (2881, 1200),
+           "TTS" : T_Tauri,
+           "AeBe" : (3077, 1282),
+           "W" : WolfRaynet,
+           "WN" : WolfRaynet,
+           "WNC" : WolfRaynet,
+           "WC" : WolfRaynet,
+           "WO" : WolfRaynet,
+           "CS" : Carbon,
+           "C" : Carbon,
+           "CN" : Carbon,
+           "CJ" : Carbon,
+           "CH" : Carbon,
+           "CHd" : Carbon,
+# I don't know about MS and S but I'm guessing similar to carbon so
+# thats what I'm going with
+           "MS" : Carbon,
+           "S" : Carbon,
+           "D" : WhiteDwarf,
+           "DA" : WhiteDwarf,
+           "DAB" : WhiteDwarf,
+           "DAO" : WhiteDwarf,
+           "DAZ" : WhiteDwarf,
+           "DAV" : WhiteDwarf,
+           "DB" : WhiteDwarf,
+           "DBZ" : WhiteDwarf,
+           "DBV" : WhiteDwarf,
+           "DO" : WhiteDwarf,
+           "DOV" : WhiteDwarf,
+           "DQ" : WhiteDwarf,
+           "DC" : WhiteDwarf,
+           "DCV" : WhiteDwarf,
+           "DX" : WhiteDwarf,
+           "N" : (54782, 22814),
+           "H" : BlackHole,
+           "SupermassiveBlackHole" : BlackHole,
+           "A_BlueWhiteSuperGiant" : GiantStar,
+           "F_WhiteSuperGiant" : GiantStar,
+           "M_RedSuperGiant" : GiantStar,
+           "M_RedGiant" : GiantStar,
+           "K_OrangeGiant" : GiantStar,
+# Planets
+           "Metal rich body" : (65045, 27102),
+           "High metal content body" : (34310, 13866),
+           "Terraformable High metal content body" : (412249, 171770),
+           "Rocky body" : (928, 500)
+           "Terraformable Rocky body" : (181104, 75460)
+           "Icy body" : IcyBody,
+           "Rocky ice body" : IcyBody,
+           "Earthlike body" : (627885, 261619),
+           "Water world" : (301410, 125587),
+           "Terraformable Water world" : (694971, 289587),
+           "Ammonia world" : (320203, 133418),
+           "Water giant" : WaterGiant,
+           "Water giant with life" : WaterGiant,
+           "Gas giant with water based life" : (2314, 964),
+           "Gas giant with ammonia based life" : (1721, 717),
+           "Sudarsky class I class giant" : (7013, 2922),
+           "Sudarsky class II class giant" : (53663, 22360),
+           "Sudarsky class III class giant" : (2693, 1122),
+           "Sudarsky class IV class giant" : (2799, 1166),
+           "Sudarsky class V class giant" : (2761, 1150),
+           "Helium gas giant" : HeGiant,
+           "Helium rich gas giant" : HeGiant
 }
 
-VALUE = { "Basic" : {},
-          "Detailed" : {}
-        }
 
 this = sys.modules[__name__]	# For holding module globals
 this.frame = None
